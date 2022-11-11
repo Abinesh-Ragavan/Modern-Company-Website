bdacb01ab4a2357e20efe7dac30db22270a895b7
Abinesh_Ragavan <abineshragavan27@gmail.com>
2022-11-11 16:42:02 +0530

    first commit da baddu

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+$('#projects-carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    nav:false,
+    dots:false,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+$('#reviews-carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    nav:false,
+    
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
